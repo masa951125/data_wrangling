bc13ce3ff85d579c26fc9d0af899ca0293ee5c5b
masa951125 <masayoshi418979@icloud.com>
2020-12-20 23:18:47 +0900

    add & cut

--- string_parsing_1.R
+++ string_parsing_1.R
@@ -1,4 +1,3 @@
-
 url <- "https://en.wikipedia.org/w/index.php?title=Gun_violence_in_the_United_States_by_state&direction=prev&oldid=810166167"
 
 murders_raw <- read_html(url) %>% 
@@ -11,11 +10,6 @@ head(murders_raw)
 class(murders_raw$population)
 class(murders_raw$total)
 
-s <- "5\'10"
-cat(s)
-s<- "5'10\""
-cat(s)
-
 library(stringr)
 
 commas <- function(x)any(str_detect(x, ","))
@@ -23,7 +17,6 @@ murders_raw %>% summarise_all(funs(commas))
 
 test_1 <- str_replace_all(murders_raw$population, ",","")
 test_1 <- as.numeric(test_1)
-
 test_2 <- parse_number(murders_raw$population)
 
 murders_new <- murders_raw %>% mutate_at(2:3, parse_number)
@@ -34,6 +27,7 @@ class(reported_heights$height)
 x <- as.numeric(reported_heights$height)
 sum(is.na(x))
 
+#reported heights
 reported_heights %>% mutate(new_heights = as.numeric(height))%>%
   filter(is.na(new_height))
 
@@ -50,9 +44,6 @@ not_inches <- function(x, smallest =50, tallest =84){
 problems <- reported_heights %>% 
   filter(not_inches(height)) %>%
   .$height
-length(problems)
-
-library(tidyverse)
 
 # detect whether a comma is present
 pattern <- ","
@@ -60,7 +51,6 @@ str_subset(murders_raw$total, pattern)
 
 # show the subset of strings including "cm"
 str_subset(reported_heights$height, "cm")
-
 
 yes <- c("180 cm", "70 inches")
 no <- c("180", "70''")
@@ -220,4 +210,193 @@ str_detect(converted, pattern)
 
 converted
 
+s <- c("5'10", "6'1")
+tab <- data.frame(x = s)
 
+tab %>% separate(x, c("feet", "inches"), sep="'")
+tab %>% extract(x, c("feet", "inches"), regex = "(\\d)'(\\d{1,2})")
+
+s <- c("5'10", "6'1\"","5'8inches")
+tab <- data.frame(x=s)
+tab %>% separate(x, c("feet", "inches"), sep="'")
+tab %>% extract(x, c("feet", "inches"), regex = "(\\d)'(\\d{1,2})")
+
+#Cases
+#1 add 0 
+yes <- c("5", "6", "5")
+no <- c("5'", "5''", "5'4")
+s <- c(yes, no)
+
+str_replace(s, "^([4-7])$", "\\1'0")
+str_replace(s, "^([4-7])'?$", "\\1'0")
+
+#meters
+yes <- c("1,7", "1, 8", "2, " )
+no <- c("5,8", "5,3,2", "1.7")
+s <- c(yes, no)
+str_replace(s, "^([12])\\s*,\\s*(\\d*)$", "\\1\\.\\2")
+
+str_trim("5 ' 9 ")
+
+#function
+convert_format <- function(s){
+  s %>%
+    str_replace("feet|foot|ft", "'") %>% #convert feet symbols to '
+    str_replace_all("inches|in|''|\"|cm|and", "") %>%  #remove inches and other symbols
+    str_replace("^([4-7])\\s*[,\\.\\s+]\\s*(\\d*)$", "\\1'\\2") %>% #change x.y, x,y x y
+    str_replace("^([56])'?$", "\\1'0") %>% #add 0 when to 5 or 6
+    str_replace("^([12])\\s*,\\s*(\\d*)$", "\\1\\.\\2") %>% #change european decimal
+    str_trim() #remove extra space
+}
+
+words_to_numbers <- function(s){
+  str_to_lower(s) %>%  
+    str_replace_all("zero", "0") %>%
+    str_replace_all("one", "1") %>%
+    str_replace_all("two", "2") %>%
+    str_replace_all("three", "3") %>%
+    str_replace_all("four", "4") %>%
+    str_replace_all("five", "5") %>%
+    str_replace_all("six", "6") %>%
+    str_replace_all("seven", "7") %>%
+    str_replace_all("eight", "8") %>%
+    str_replace_all("nine", "9") %>%
+    str_replace_all("ten", "10") %>%
+    str_replace_all("eleven", "11")
+}
+
+converted <- problems %>% words_to_numbers %>% convert_format
+remaining_problems <- converted[not_inches_or_cm(converted)]
+pattern <- "^[4-7]\\s*'\\s*\\d+\\.?\\d*$"
+index <- str_detect(remaining_problems, pattern)
+sum(index)
+remaining_problems[!index]
+
+
+pattern <- "^([4-7])\\s*'\\s*(\\d+\\.?\\d*)$"
+
+smallest <- 50
+tallest <- 84
+new_heights <- reported_heights %>% 
+  mutate(original = height, 
+         height = words_to_numbers(height) %>% convert_format()) %>%
+  extract(height, c("feet", "inches"), regex = pattern, remove = FALSE) %>% 
+  mutate_at(c("height", "feet", "inches"), as.numeric) %>%
+  mutate(guess = 12*feet + inches) %>%
+  mutate(height = case_when(
+    !is.na(height) & between(height, smallest, tallest) ~ height, #inches 
+    !is.na(height) & between(height/2.54, smallest, tallest) ~ height/2.54, #centimeters
+    !is.na(height) & between(height*100/2.54, smallest, tallest) ~ height*100/2.54, #meters
+    !is.na(guess) & inches < 12 & between(guess, smallest, tallest) ~ guess, #feet'inches
+    TRUE ~ as.numeric(NA))) %>%
+  select(-guess)
+
+new_heights %>%
+  filter(not_inches(original)) %>%
+  select(original, height) %>% 
+  arrange(height) %>%
+  View()
+
+new_heights %>% arrange(height) %>% head(n=7)
+
+#data spliting
+
+filename <- system.file("extdata/murders.csv", package = "dslabs")
+lines <- readLines(filename)
+lines %>% head()
+
+x <- str_split(lines, ",")
+library(purrr)
+map(x, function(y)y[1])
+map(x,1)
+col_names <- x[[1]]
+
+dat <- data.frame(map_chr(x,1),
+                  map_chr(x,2),
+                  map_chr(x,3),
+                  map_chr(x,4),
+                  map_chr(x,5))%>%
+  mutate_all(parse_guess)%>%
+  setNames(col_names)
+
+dat <- tibble(dat)
+
+x <- str_split(lines, ",", simplify = T)
+col_names <- x[1,]
+
+new_x <- x %>% as_tibble() %>%
+  setNames(col_names) %>%
+  mutate_all(parse_guess)
+new_x 
+
+#Extracting a table from PDF
+
+library(dslabs)
+data("research_funding_rates")
+research_funding_rates 
+
+library("pdftools")
+temp_file <- tempfile()
+url <- "http://www.pnas.org/content/suppl/2015/09/16/1510159112.DCSupplemental/pnas.201510159SI.pdf"
+download.file(url, temp_file,mode = "wb")  #include mode="wb"
+txt <- pdf_text(temp_file)
+file.remove(temp_file)
+
+raw_data_research_funding_rates <- txt[2]
+# data("raw_data_research_funding_rates")
+
+raw_data_research_funding_rates %>% head
+tab <- str_split(raw_data_research_funding_rates, "\n")
+tab <- tab[[1]]
+tab %>% head
+
+the_names_1 <- tab[3]
+the_names_2 <- tab[4]
+
+the_names_1 <- the_names_1 %>%
+  str_trim() %>%
+  str_replace_all(",\\s.", "") %>%
+  str_split("\\s{2,}", simplify = TRUE)
+the_names_1
+
+the_names_2 <- the_names_2 %>%
+  str_trim() %>%   
+  str_split("\\s+", simplify = TRUE)
+the_names_2
+
+tmp_names <- str_c(rep(the_names_1, each = 3), the_names_2[-1], sep = "_")
+the_names <- c(the_names_2[1], tmp_names) %>%
+ str_to_lower() %>%
+  str_replace_all("\\s", "_")
+the_names
+
+new_research_funding_rates <- tab[6:14] %>%
+  str_trim %>%
+  str_split("\\s{2,}", simplify = TRUE) %>%
+  data.frame(stringsAsFactors = FALSE) %>%
+  setNames(the_names) %>%
+  mutate_at(-1, parse_number)
+new_research_funding_rates %>% head()
+
+identical(new_research_funding_rates, research_funding_rates)
+
+library(dslabs)
+data("gapminder")
+gapminder %>% 
+  filter(region=="Caribbean") %>%
+  ggplot(aes(year, life_expectancy, color = country)) +
+  geom_line()
+
+gapminder %>% 
+  filter(region=="Caribbean") %>%
+  filter(str_length(country) >= 12) %>%
+  distinct(country) 
+
+gapminder %>% filter(region=="Caribbean") %>%
+  mutate(country = recode(country, 
+                          'Antigua and Barbuda'="Barbuda",
+                          'Dominican Republic' = "DR",
+                          'St. Vincent and the Grenadines' = "St. Vincent",
+                          'Trinidad and Tobago' = "Trinidad")) %>%
+  ggplot(aes(year, life_expectancy, color = country)) +
+  geom_line()
